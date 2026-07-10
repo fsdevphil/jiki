04a23dcea36ac8d7678347ad603b3ee931a8514a
Phil <279342427+fsdevphil@users.noreply.github.com>
2026-07-10 13:51:27 +0100

    Create 33-bouncer-wristbands.js

---
+++ 33-bouncer-wristbands.js
@@ -0,0 +1,12 @@
+// Give out wristbands
+let age = getAge()
+
+if (age < 13) {
+  giveChildWristband()
+} else if (age < 18) {
+  giveTeenWristband()
+} else if (age < 65) {
+  giveAdultWristband()
+} else {
+  giveSeniorWristband()
+}
